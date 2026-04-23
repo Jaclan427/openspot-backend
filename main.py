48bebac5d1291335a7986d864f76d9447631d913
Jack Lancaster <jaclan427@gmail.com>
2026-04-23 11:12:22 -0400

    dfvvfdvdvdfvd

--- main.py
+++ main.py
@@ -11,15 +11,16 @@ app = FastAPI(
 )
 
 origins = [
-    "http://localhost:3000/",
-    "http://127.0.0.1:3000/",
-    "https://sensors-ivory.vercel.app/",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://sensors-ivory.vercel.app",
+    "https://sensors-git-sayeds-branch-imranali17689s-projects.vercel.app",
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_origin_regex=r"https://.%2A/.vercel/.app",
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=[""],
     allow_headers=[""],
