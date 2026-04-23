a24f725d9633925fd4a871a5348897972f933754
Jack Lancaster <jaclan427@gmail.com>
2026-04-23 11:02:27 -0400

    dhvfhf

--- main.py
+++ main.py
@@ -14,13 +14,12 @@ origins = [
     "http://localhost:3000/",
     "http://127.0.0.1:3000/",
     "https://sensors-ivory.vercel.app/",
-    "https://sensors-git-sayeds-branch-imranali17689s-projects.vercel.app/",
-    "https://sensors-nwhaonw2o-imranali17689s-projects.vercel.app/",
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex=r"https://.%2A/.vercel/.app",
     allow_credentials=True,
     allow_methods=[""],
     allow_headers=[""],
